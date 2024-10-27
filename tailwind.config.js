0af62c84c1b2552e02290a99a9f7163f0419f1ca
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2024-10-27 19:53:27 +0400

    Tailwind Config Clean Up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
     extend: {
       colors: {
+        red: "hsl(4, 69%, 50%)",
         lime: "hsl(61, 70%, 52%)",
         "lime-900": "hsl(202,55%,12%)",
-        red: "hsl(4, 69%, 50%)",
-        white: "hsl(0, 0%, 100%)",
         "slate-100": "hsl(202, 86%, 94%)",
         "slate-300": "hsl(203, 41%, 72%)",
         "slate-500": "hsl(200, 26%, 54%)",
