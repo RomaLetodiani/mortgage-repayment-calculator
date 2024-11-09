d3d0b5ad4fb98bacf45a7de581a93acb598b3682
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2024-11-09 20:58:48 +0400

    Enhance EmptyRender component with illustration and informative text

--- src/components/MainCard/components/rightSide/components/Empty.render.tsx
+++ src/components/MainCard/components/rightSide/components/Empty.render.tsx
@@ -1,5 +1,20 @@
+import illustrationEmpty from "@/assets/images/illustration-empty.svg";
+
 const EmptyRender = () => {
-  return <div>EmptyRender</div>;
+  return (
+    <div>
+      <div>
+        <img src={illustrationEmpty} alt="Empty" />
+      </div>
+      <div>
+        <h3>Results shown here</h3>
+        <p>
+          Complete the form and click "calculate repayments" to see what your monthly repayments
+          would be.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default EmptyRender;
