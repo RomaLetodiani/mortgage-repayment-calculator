7dc5a722440c0fed17fc996f666a39783bd4f311
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2024-11-12 22:34:51 +0400

    Enhance RightSide component to conditionally render based on search parameters

--- src/components/MainCard/components/rightSide/Right.side.tsx
+++ src/components/MainCard/components/rightSide/Right.side.tsx
@@ -1,11 +1,14 @@
+import useSearchParams from "@/hooks/useSearchParams";
 import CompletedRender from "./components/Completed.render";
 import EmptyRender from "./components/Empty.render";
 
 const RightSide = () => {
+  const [searchParams] = useSearchParams();
+  const completed = searchParams.completed === "true";
+
   return (
-    <div className="p-10 bg-slate-900 rounded-bl-[4.5rem]">
-      <CompletedRender />
-      <EmptyRender />
+    <div className="px-5 py-6 sm:p-10 bg-slate-900 sm:rounded-bl-[4.5rem]">
+      {completed ? <CompletedRender /> : <EmptyRender />}
     </div>
   );
 };
