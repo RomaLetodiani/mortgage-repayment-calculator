20b371c2c32d86fab1d7047dc608e3ae14e2611e
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2024-11-12 21:43:42 +0400

    Enhance LeftSideFooter component with calculator button and styling

--- src/components/MainCard/components/leftSide/components/LeftSide.footer.tsx
+++ src/components/MainCard/components/leftSide/components/LeftSide.footer.tsx
@@ -1,5 +1,17 @@
+import calculatorSVG from "@/assets/images/icon-calculator.svg";
+
 const LeftSideFooter = () => {
-  return <div>LeftSideFooter</div>;
+  return (
+    <div>
+      <button
+        type="submit"
+        className="rounded-full bg-lime hover:scale-105 py-2 px-5 flex items-center font-bold transition-transform duration-300 ease-in-out"
+      >
+        <img src={calculatorSVG} alt="Calculator" className="w-5 h-5 inline-block mr-2" />
+        <span>Calculate Repayments</span>
+      </button>
+    </div>
+  );
 };
 
 export default LeftSideFooter;
