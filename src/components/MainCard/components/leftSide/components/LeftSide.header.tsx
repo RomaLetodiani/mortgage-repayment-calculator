9ccc519c3bedbd24da9f6a27b9780ebdde03cfb9
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2024-11-12 21:43:36 +0400

    Enhance LeftSideHeader component with improved layout and styling

--- src/components/MainCard/components/leftSide/components/LeftSide.header.tsx
+++ src/components/MainCard/components/leftSide/components/LeftSide.header.tsx
@@ -1,5 +1,13 @@
 const LeftSideHeader = () => {
-  return <div>Header</div>;
+  return (
+    <div className="flex gap-2 justify-between flex-wrap">
+      <h1 className="text-lg font-bold">Mortgage Calculator</h1>
+
+      <button className="text-sm">
+        <span className="underline font-semibold text-slate-500">Clear All</span>
+      </button>
+    </div>
+  );
 };
 
 export default LeftSideHeader;
