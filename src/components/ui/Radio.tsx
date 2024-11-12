8b6f39a087cdac21e5e227a263685b63b6955628
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2024-11-13 01:31:52 +0400

    Enhance Radio component with customizable wrapper and label classes for improved styling

--- src/components/ui/Radio.tsx
+++ src/components/ui/Radio.tsx
@@ -1,15 +1,38 @@
 import { InputHTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface RadioProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
+  wrapperClassName?: string;
+  labelClassName?: string;
 }
 
-const Radio = ({ label, ...props }: RadioProps) => {
+const Radio = ({
+  label,
+  className,
+  labelClassName,
+  wrapperClassName,
+  ...props
+}: RadioProps) => {
   return (
-    <div>
-      <label htmlFor={props.id} className="flex gap-5 rounded border px-3 py-1">
-        <input type="radio" {...props} />
-        <span>{label}</span>
+    <div className={twMerge("my-2", wrapperClassName)}>
+      <label
+        htmlFor={props.id}
+        className={twMerge(
+          "flex cursor-pointer items-center gap-5 rounded border border-slate-900 px-4 py-2",
+          props.checked ? "border-lime bg-lime/10" : "",
+          labelClassName,
+        )}
+      >
+        <input
+          type="radio"
+          className={twMerge(
+            "cursor-pointer text-lime focus:ring-lime",
+            className,
+          )}
+          {...props}
+        />
+        <span className="text-sm font-semibold text-slate-900">{label}</span>
       </label>
     </div>
   );
