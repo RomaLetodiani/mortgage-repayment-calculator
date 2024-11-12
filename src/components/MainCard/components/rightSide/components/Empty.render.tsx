9a6966c87263039dd42cf784a500c6a7c02f6e61
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2024-11-12 22:34:35 +0400

    Enhance Empty.render component with improved layout and styling

--- src/components/MainCard/components/rightSide/components/Empty.render.tsx
+++ src/components/MainCard/components/rightSide/components/Empty.render.tsx
@@ -2,13 +2,13 @@ import illustrationEmpty from "@/assets/images/illustration-empty.svg";
 
 const EmptyRender = () => {
   return (
-    <div>
-      <div>
+    <div className="flex justify-center flex-col">
+      <div className="flex justify-center">
         <img src={illustrationEmpty} alt="Empty" />
       </div>
-      <div>
-        <h3>Results shown here</h3>
-        <p>
+      <div className="text-center">
+        <h3 className="text-white font-bold my-3">Results shown here</h3>
+        <p className="text-slate-100 text-sm opacity-70">
           Complete the form and click "calculate repayments" to see what your monthly repayments
           would be.
         </p>
